47a26586d5212319a62bade7df37a20ad466f1b3
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-02-26 23:29:13 -0300

    Update LISTA2.c

--- LISTA2.c
+++ LISTA2.c
@@ -1,4 +1,44 @@
+-------------EXEMPLO 2
+#include <stdio.h>
+#include "pilha.h"
 
+int main(void){
+	int n;
+	Pilha P = pilha(8* sizeof(int));
+	printf("Decimal? ");
+	scanf("%d", &n);
+	do {
+		empilha(n % 2, P);
+		n /= 2;
+	} while (n > 0);
+	printf("Binario: ");
+
+	while (!vaziap(P))
+		printf("%d", desempilha(P));
+		printf("\n");
+	destroip(&P);
+	return 0;
+}
+-----------EXEMPLO 3
+
+#include <stdio.h>
+#include "pilha.h"
+
+int main(void){
+	char c[513];
+	Pilha P = pilha(513);
+	printf("Cadeia? ");
+	gets(c);
+	for (int i = 0; c[i]; i++)
+		empilha(c[i], P);
+	printf("Inverso: ");
+	while (!vaziap(P))
+		printf("%c", desempilha(P));
+	puts("\n");
+	destroip(&P);
+	return 0;
+}
+************************************************************
 1.Ordenação crescente
 --------------------------------------------------------------
 2.Ordenação decrescente e sem repetição
