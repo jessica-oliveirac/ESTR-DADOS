b85c0b934af193217cebddd91ec4a1ef529cdb97
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-03-16 21:08:34 -0300

    Create LISTA7.c

---
+++ LISTA7.c
@@ -0,0 +1,23 @@
+1.	Teste da função bsort()
+-------------------------------------------
+2.	A função empurra()
+-------------------------------------------
+3.	Versão recursiva de bubble sort
+-------------------------------------------
+4.	Teste da função intercala()
+-------------------------------------------
+5.	Teste da função msort()
+-------------------------------------------
+6.	Preenchimento aleatório
+-------------------------------------------
+7.	Comparação entre bsort()e msort()
+-------------------------------------------
+8.	Desempenho de msort()para vetores muito grandes
+-------------------------------------------
+9.	Teste da função lsearch()
+-------------------------------------------
+10.	Teste da função bsearch()
+-------------------------------------------
+11.	Versão recursiva de busca linear
+-------------------------------------------
+12.	Versão recursiva de busca binária
