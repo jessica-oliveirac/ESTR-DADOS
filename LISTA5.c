4818f61d5b0eabe48bf7af356dc6ba7a23e5b094
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-03-16 21:00:35 -0300

    Create LISTA5.c

---
+++ LISTA5.c
@@ -0,0 +1,6 @@
+PARTE 1
+1.	Programa para representação e exibição da imagem
+-----------------------------------------------------------------------------
+PARTE 2
+#1.	Correção do programa
+#2.	Entrada de arquivo
