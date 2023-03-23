d81523c47754591f170f48e1c04f8d01b007b4cf
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-03-23 17:39:20 -0300

    Update and rename LISTA6.c to LISTA6(Recursividade.c

--- LISTA6(Recursividade.c
+++ LISTA6(Recursividade.c
@@ -1,4 +1,17 @@
 1.	Cálculo de fatorial
+
+#include <stdio.h>
+int fat(int n) {
+	if(n==0)return 1;
+	return n*fat(n-1);
+}
+int main(void) {
+	int n;
+	printf("Num? ");
+	scanf("%d",&n);
+	printf("Fat = %d\n",fat(n));
+	return 0;
+}
 -------------------------------------------------------------------------
 2.	Cálculo de potência
 -------------------------------------------------------------------------
