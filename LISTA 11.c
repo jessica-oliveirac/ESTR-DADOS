1d01963a4e01b089324be8aaabcead1c42519fc5
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-04-20 20:28:25 -0300

    Create LISTA 11.c

---
+++ LISTA 11.c
@@ -0,0 +1,7 @@
+1.Cadastro de clientes
+2.Teste da implementação
+--------------------------------------------------------------
+3.Tradução de inglês para português
+--------------------------------------------------------------
+4.Teste da implementação
+
