26c867004b242abd8ed054f54309967b0182c729
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-04-20 21:00:39 -0300

    Update LISTA 10.c

--- LISTA 10.c
+++ LISTA 10.c
@@ -53,6 +53,65 @@ int main(void) {
 2.Inserção recursiva
 --------------------------------------------------------
 3.Remoção
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int Chave;
+typedef char Valor[22];
+typedef struct map{
+	Chave chave;
+	Valor valor;
+	struct map *prox;
+} *Map;
+
+Map no_map(Chave c,Valor v,Map p) {
+	Map n = malloc(sizeof(struct map));
+	n->chave = c;
+	strcpy(n->valor,v);
+	n->prox = p;
+	return n;
+}
+
+void insm(Chave c,Valor v,Map *M){
+	while(*M && c>(*M)->chave )
+		M = &(*M)->prox;
+	if( *M && c ==(*M)->chave )
+		strcpy((*M)->valor,v);
+	else *M = no_map(c,v,*M);
+}
+
+void exibem(Map M) {
+	printf("[");
+	while(M ) {
+		printf("(%d,%s)",M->chave,M->valor);
+		if(M->prox)printf(",");
+		M = M->prox;
+	}
+	printf("]\n");
+}
+
+void remm(Chave c, Map *M) {
+	while(*M && c>(*M)->chave )
+		M = &(*M)->prox;
+	if( *M==NULL ||c!=(*M)->chave ) return;
+	Map n = *M;
+	*M = n->prox;
+	free(n);
+}
+
+int main(void) {
+	Map I = NULL;
+	insm(36,"Leo",&I); insm(15,"Ivo",&I);
+	insm(42,"Eva",&I); insm(29,"Ana",&I);
+	exibem(I);
+	remm(29,&I);
+	exibem(I);
+	remm(42,&I);
+	exibem(I);
+	return 0;
+}
 --------------------------------------------------------
 4.Remoção recursiva
 --------------------------------------------------------
