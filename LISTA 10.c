a4db0f2a89b1ca49639be016e245a0df1960ba76
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-04-20 21:18:18 -0300

    Update LISTA 10.c

--- LISTA 10.c
+++ LISTA 10.c
@@ -116,6 +116,63 @@ int main(void) {
 4.Remoção recursiva
 --------------------------------------------------------
 5.Pertinência
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int Chave;
+typedef char Valor[22];
+typedef struct map{
+	Chave chave;
+	Valor valor;
+	struct map *prox;
+} *Map;
+
+Map no_map(Chave c,Valor v,Map p) {
+	Map n = malloc(sizeof(struct map));
+	n->chave = c;
+	strcpy(n->valor,v);
+	n->prox = p;
+	return n;
+}
+
+void insm(Chave c,Valor v,Map *M){
+	while(*M && c>(*M)->chave )
+		M = &(*M)->prox;
+	if( *M && c ==(*M)->chave )
+		strcpy((*M)->valor,v);
+	else *M = no_map(c,v,*M);
+}
+
+void exibem(Map M) {
+	printf("[");
+	while(M ) {
+		printf("(%d,%s)",M->chave,M->valor);
+		if(M->prox)printf(",");
+		M = M->prox;
+	}
+	printf("]\n");
+}
+
+int pertm(Chave c,Valor v,Map M) {
+ while(M && c>M->chave )
+ M = M->prox;
+ if(M && c==M->chave )
+ strcpy(v,M->valor);
+ return(M && c==M->chave);
+}
+int main(void) {
+	Valor w;
+	Map I = NULL;
+	insm(36,"Leo",&I); insm(15,"Ivo",&I); insm(42,"Eva",&I);
+	exibem(I);
+	if(pertm(42,w,I)) printf("Valor da chave 42: %s\n",w);
+	else puts("Chave 42 inexistente!");
+	if(pertm(10,w,I)) printf("Valor da chave 10: %s\n",w);
+	else puts("Chave 10 inexistente!");
+	return 0;
+}
 --------------------------------------------------------
 6.Pertinência recursiva
 --------------------------------------------------------
