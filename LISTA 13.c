2cc731464ce245324321af87c9d7484145259cbb
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-05-04 22:36:40 -0300

    Update LISTA 13.c

--- LISTA 13.c
+++ LISTA 13.c
@@ -140,6 +140,59 @@ int main(void) {
 }
 ----------------------------------------------------------------------------------
 4.	Busca em árvore de busca binária
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Item;
+typedef struct arv{
+	struct arv*esq;
+	Item item;
+	struct arv *dir;
+} *Arv;
+
+Arv arv(Arv e,Item x,Arv d) {
+	Arv n = malloc(sizeof(struct arv));
+	n->esq = e;
+	n->item = x;
+	n->dir = d;
+	return n;
+}
+
+void ins(Item x, Arv*A) {
+	if(*A ==NULL ) *A = arv(NULL,x,NULL);
+	else if(x <=(*A)->item )ins(x,&(*A)->esq);
+	else ins(x,&(*A)->dir);
+}
+
+void emordem(Arv A,int n) {
+	if(A==NULL )return;
+	emordem(A->dir,n+1);
+	printf("%*s%d\n",3*n,"",A->item);
+	emordem(A->esq,n+1);
+}
+int busca(Item x, Arv A) {
+	if( A == NULL) return 0;
+	if( x == A->item) return 1;
+	if( x < A->item) return busca(x,A->esq);
+	else return busca(x,A->dir);
+}
+int main(void) {
+	int v[9] = {71,43,64,92,80,27,58,35,16};
+	Arv A = NULL;
+	for(int i=0;i<9;i++)
+		ins(v[i],&A);
+	emordem(A,9);
+	puts("\nPara sair, digite um inteiro negativo!");
+	while(1) {
+		int x;
+		printf("\nItem a ser buscado? ");
+		scanf("%d",&x);
+		if(x<0)break;
+		if(busca(x,A))puts("Encontrado!");
+		else puts("Inexistente!");
+	}
+	return 0;
+}
 ----------------------------------------------------------------------------------
 5.	Remoção de itens máximos
 #include <stdio.h>
