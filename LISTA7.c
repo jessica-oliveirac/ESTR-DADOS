920b2799350cb87b36f792bacf755110d02e4492
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-06-01 22:19:54 -0300

    Update LISTA7.c

--- LISTA7.c
+++ LISTA7.c
@@ -30,6 +30,31 @@ int main(void){
 3.	Versão recursiva de bubble sort
 -------------------------------------------
 4.	Teste da função intercala()
+#include <stdio.h>
+#include <stdlib.h>
+void intercala(int v[],int p,int m,int u) {
+	int *w = malloc((u-p+1)*sizeof(int));
+	int i=p,j=m+1,k=0;
+	while(i<=m && j<=u )
+		w[k++]=(v[i]<v[j])? v[i++]:v[j++];
+	while(i<=m ) w[k++]= v[i++];
+	while(j<=u ) w[k++]= v[j++];
+	for(k=0; k<=u-p; k++)v[p+k]= w[k];
+	free(w);
+}
+void exibe(int v[], int n){
+	for(int i=1; i<n; i++)
+     printf("%d ", v[i]);
+}
+int main(void){
+	int v[8] ={31,48,60,80,19,27,52,75};
+	intercala(v,0,3,7);
+	exibe(v,8);
+	int w[9] ={10,82,27,38,41,53,60,75,99};
+	intercala(w,0,1,8);
+	exibe(w,9);
+	return 0;
+}
 -------------------------------------------
 5.	Teste da função msort()
 -------------------------------------------
