e668dc36a9b706e0a123e5c7d1ce3a1767caca90
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-06-01 22:30:27 -0300

    Update LISTA7.c

--- LISTA7.c
+++ LISTA7.c
@@ -57,6 +57,39 @@ int main(void){
 }
 -------------------------------------------
 5.	Teste da função msort()
+#include <stdio.h>
+#include <stdlib.h>
+void intercala(int v[],int p,int m,int u) {
+	int *w = malloc((u-p+1)*sizeof(int));
+	int i=p,j=m+1,k=0;
+	while(i<=m && j<=u )
+		w[k++]=(v[i]<v[j])? v[i++]:v[j++];
+	while(i<=m ) w[k++]= v[i++];
+	while(j<=u ) w[k++]= v[j++];
+	for(k=0; k<=u-p; k++)v[p+k]= w[k];
+	free(w);
+}
+void ms(int v[],int p,int u) {
+	if(p==u )return;
+	int m = (p+u)/2;
+	ms(v,p,m);
+	ms(v,m+1,u);
+	intercala(v,p,m,u);
+}
+void msort(int v[],int n) {
+	ms(v,0,n-1);
+}
+
+void exibe(int v[], int n){
+	for(int i=1; i<n; i++)
+     printf("%d ", v[i]);
+}
+int main(void) {
+	int v[10]={83,31,91,46,27,20,96,25,96,80};
+	msort(v,10);
+	exibe(v,10);
+	return 0;
+}
 -------------------------------------------
 6.	Preenchimento aleatório
 -------------------------------------------
