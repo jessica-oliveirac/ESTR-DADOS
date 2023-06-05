4257183c169564a2896076b37c239afb4d3b746e
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-06-05 01:06:36 -0300

    Update 9 - Listas Ordenadas.c

--- 9 - Listas Ordenadas.c
+++ 9 - Listas Ordenadas.c
@@ -47,10 +47,103 @@ int main(void) {
 6 - Exibição recursiva inversa
 ------------------------------------------------------------------------------------
 7 - Programa para remoção em lista ordenada
+#include <stdio.h>
+#include <stdlib.h>
+typedef int Item;
+typedef struct no {
+	Item item;
+	struct no *prox;
+} *Lista;
+
+Lista no(Item x,Lista p){
+	Lista n = malloc(sizeof(struct no));
+	n->item =x;
+	n->prox =p;
+	return n;
+}
+void ins(Item x,Lista *L){
+	while( *L != NULL && (*L)->item < x)
+		L = &(*L)->prox;
+	*L = no(x,*L);
+}
+
+void rem(Item x,Lista *L){
+	while( *L != NULL &&(*L)->item <x )
+		L = &(*L)->prox;
+	if(*L ==NULL ||(*L)->item >x )return;
+	Lista n = *L;
+	*L =n->prox;
+	free(n);
+}
+void exibe(Lista I){
+	while(I != NULL){
+		printf("%d ", I->item);
+		I = I->prox;
+	}
+}
+int main(void){
+	Lista I = NULL;
+	ins(4,&I); 
+	ins(1,&I); 
+	ins(3,&I); 
+	ins(5,&I); 
+	ins(2,&I);
+	rem(3,&I);
+	exibe(I);
+	return 0;
+}
 ------------------------------------------------------------------------------------
 8 - Remoção de todas as ocorrências
 ------------------------------------------------------------------------------------
 9 - Programa para verificação de pertinência em lista ordenada
+#include <stdio.h>
+#include <stdlib.h>
+typedef int Item;
+typedef struct no{
+	Item item;
+	struct no *prox;
+} *Lista;
+
+Lista no(Item x,Lista p){
+	Lista n = malloc(sizeof(struct no));
+	n->item =x;
+	n->prox =p;
+	return n;
+}
+void ins(Item x,Lista *L){
+	while( *L != NULL && (*L)->item < x)
+		L = &(*L)->prox;
+	*L = no(x,*L);
+}
+void rem(Item x,Lista *L){
+	while( *L != NULL &&(*L)->item <x)
+		L = &(*L)->prox;
+	if(*L ==NULL ||(*L)->item >x)return;
+	Lista n = *L;
+	*L =n->prox;
+	free(n);
+}
+int pert(Item x,Lista L){
+	while(L !=NULL && L->item <x)
+		L = L->prox;
+	return(L != NULL &&L->item == x);
+}
+void exibe(Lista I){
+	while(I != NULL){
+		printf("%d ", I->item);
+		I = I->prox;
+	}
+}
+int main(void){
+	Lista I = NULL;
+	ins(4,&I);
+	ins(1,&I);
+	ins(3,&I);
+	ins(2,&I);
+	printf("%d\n",pert(5,I));
+	printf("%d\n",pert(3,I));
+	return 0;
+}
 ------------------------------------------------------------------------------------
 
 10 - Verificação de pertinência recursiva
