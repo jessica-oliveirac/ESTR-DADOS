1138d8fde1dd7479128db17499775a45beebe5de
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-06-05 00:31:36 -0300

    Update LISTA 8.c

--- LISTA 8.c
+++ LISTA 8.c
@@ -60,14 +60,172 @@ int main(void) {
 }
 ------------------------------------------------------------------------------------
 3 - Programa para tamanho
+#include <stdio.h>
+#include <stdlib.h>
+typedef int Item;
+typedef struct no{
+	Item item; 
+	struct no *prox;
+} *Lista;
+
+int tamanho(Lista L){
+	int t =0;
+	while(L ) {
+	t++;
+	L =L->prox;
+	}
+	return t;
+}
+Lista no(Item x, Lista p){
+	Lista n = malloc(sizeof(struct no));
+	n->item = x;
+	n->prox = p;
+	return n;
+}
+void exibe(Lista I){
+	while(I != NULL){
+		printf("%d\n", I->item);
+		I = I->prox;
+	}
+}
+int main(void) {
+	Lista I = no(3, no(1, no(5, NULL)));
+	exibe(I);
+	printf("\nTamanho = %d",tamanho(I));
+	return 0;
+}
 ------------------------------------------------------------------------------------
 4 - Soma dos itens da lista
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Item;
+typedef struct no{
+	Item item; 
+	struct no *prox;
+} *Lista;
+
+Lista no(Item x, Lista p){
+	Lista n = malloc(sizeof(struct no));
+	n->item = x;
+	n->prox = p;
+	return n;
+}
+
+void exibe(Lista L){
+	printf("[");
+	while(L != NULL) {
+		printf("%d", L->item);
+ 		if(L->prox != NULL){
+			printf(", ");
+		}
+    L = L->prox;
+ 	}
+  printf("]");
+}
+
+int soma(Lista L){
+    int soma = 0;
+    while(L != NULL){
+	soma += L->item; 
+	L = L->prox;
+		}
+    return soma;
+}
+int tamanho(Lista L){
+	int t =0;
+	while(L ) {
+	t++;
+	L =L->prox;
+	}
+	return t;
+}
+int main(void) {
+	Lista I = no(3, no(1, no(5, NULL)));
+	exibe(I);
+	printf("\nSoma = %d", soma(I));
+	printf("\nTamanho = %d",tamanho(I));
+	return 0;
+}
 ------------------------------------------------------------------------------------
 5 - Programa para lista aleatória
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+typedef int Item;
+typedef struct no{
+	Item item; 
+	struct no *prox;
+} *Lista;
+
+Lista no(Item x, Lista p){
+	Lista n = malloc(sizeof(struct no));
+	n->item = x;
+	n->prox = p;
+	return n;
+}
+Lista aleatoria(int n,int m){
+	Lista L = NULL;
+	while(n>0 ) {
+		L = no(rand()%m,L);
+		n--;
+	}
+	return L;
+}
+void exibe(Lista I){
+	while(I != NULL){
+		printf("%d\n", I->item);
+		I = I->prox;
+	}
+}
+int main(void){
+	srand(time(NULL));
+	Lista A = aleatoria(10,100);
+	exibe(A);
+	return 0;
+}
 ------------------------------------------------------------------------------------
 6 - Criação de lista contendo um intervalo
 ------------------------------------------------------------------------------------
 7 - Programa para anexação
+#include <stdio.h>
+#include <stdlib.h>
+typedef int Item;
+typedef struct no{
+	Item item; 
+	struct no *prox;
+} *Lista;
+
+Lista no(Item x, Lista p){
+	Lista n = malloc(sizeof(struct no));
+	n->item = x;
+	n->prox = p;
+	return n;
+}
+void anexa(Lista *A,Lista B){
+	if(!B )return;
+	while(*A )
+		A = &(*A)->prox;
+	*A = B;
+}
+void exibe(Lista I){
+	while(I != NULL){
+		printf("%d\n", I->item);
+		I = I->prox;
+	}
+}
+int	main(void){
+	Lista H = no(4,no(2,NULL));
+	Lista I = no(3,no(1,no(5,NULL)));
+	printf("H = "); exibe(H);
+	printf("I = "); exibe(I);
+	printf("Pressione enter");
+	getchar();
+	anexa(&H,I);
+	printf("H = "); exibe(H);
+	printf("I = "); exibe(I);
+	return 0;
+}
 ------------------------------------------------------------------------------------
 8 - Último item
 ------------------------------------------------------------------------------------
