15f538eb0a989acc30bd9e6b49a76850087533f2
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-06-05 00:37:12 -0300

    Update and rename LISTA 9.c to 9 - Listas Ordenadas.c

--- 9 - Listas Ordenadas.c
+++ 9 - Listas Ordenadas.c
@@ -1,4 +1,40 @@
 1 - Programa para inserção em lista ordenada
+#include <stdio.h>
+#include <stdlib.h>
+typedef int Item;
+typedef struct no {
+	Item item;
+	struct no *prox;
+} *Lista;
+
+Lista no(Item x,Lista p) {
+	Lista n = malloc(sizeof(struct no));
+	n->item =x;
+	n->prox =p;
+	return n;
+}
+void ins(Item x,Lista *L) {
+	while( *L != NULL && (*L)->item < x)
+		L = &(*L)->prox;
+	*L = no(x,*L);
+}
+void exibe(Lista I){
+	while(I != NULL){
+		printf("%d ", I->item);
+		I = I->prox;
+	}
+}
+int main(void) {
+	Lista I =NULL;
+	ins(4,&I);
+	ins(1,&I);
+	ins(3,&I);
+	ins(5,&I);
+	ins(2,&I);
+	exibe(I);
+	return 0;
+}
+
 ------------------------------------------------------------------------------------
 2 - Inserção iterativa sem repetição
 ------------------------------------------------------------------------------------
